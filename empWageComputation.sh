4bfb3654e132f2fcd01d890288bdfc1acbf73daf
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-17 18:15:42 +0530

    Second Use Case, daily wage calculation completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,4 @@
-echo "  Welcome to Employee Wage Computation Program"
+echo " Welcome to Employee Wage Computation Program"
 
 isPresent=1
 isAbsent=0
@@ -6,7 +6,13 @@ attendance=$((RANDOM % 2))
 if [ $attendance == 1 ];
 then
 	echo "Employee Present"
+	empHours=8
 else
 	echo "Empolyee Absent"
+	empHours=0
 fi
 echo "completed the first use case"
+
+empRatePerHour=20
+salary=$(($empHours * $empRatePerHour))
+echo "Employe Day's salaryis:" $salary

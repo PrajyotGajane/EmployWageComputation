eb591a443ac387c55d273533bcdc5ceb75f2d815
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-17 21:25:04 +0530

    Employee Part Time Wage logic added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 echo " Welcome to Employee Wage Computation Program"
 
 isPresent=1
@@ -16,3 +17,22 @@ echo "completed the first use case"
 empRatePerHour=20
 salary=$(($empHours * $empRatePerHour))
 echo "Employe Day's salaryis:" $salary
+echo "completed seconod use case"
+
+isFullTime=1
+isPartTime=2
+attenDayWage=$((RANDOM % 3))
+
+case $attenDayWage in
+	$isFullTime)
+				;;
+	$isPartTime)
+			empHours=4
+			;;
+	*)
+			empHours=0
+			;;
+esac
+
+salary=$(($empHours * $empRatePerHour))
+echo "Salary :" $salary

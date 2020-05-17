8b6d8f5fd1289752ad587c7a45e8dd1cef695d2e
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-17 17:58:48 +0530

    First Use Case completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,12 @@
 echo "  Welcome to Employee Wage Computation Program"
 
+isPresent=1
+isAbsent=0
+attendance=$((RANDOM % 2))
+if [ $attendance == 1 ];
+then
+	echo "Employee Present"
+else
+	echo "Empolyee Absent"
+fi
+echo "completed the first use case"
